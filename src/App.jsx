b9f852dab130f91365599fb59ad6576822e775cb
slopez-2018151 <slopez-2018151@kinal.edu.gt>
2023-05-26 11:50:37 -0600

    Footer y sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import { Footer } from './Components/Footer/Footer.jsx'
+import './Components/CSS/style.css'
+import { Sidebar } from './Components/Sidebar/Sidebar.jsx'
 
 export const App = () => {
   return (
     <>
-    <h1>HUFNDFOSDFDS</h1>
+    <Sidebar />
     <Outlet />
+    <Footer />
     </>
   )
 }
