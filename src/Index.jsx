9a3c34db4e59fbd832c2818a27b6234da65a662b
pgarcia-2019596 <pgarcia-2019596@kinal.edu.gt>
2023-06-12 10:47:37 -0600

    LoginPage y UserPage arregladas

--- src/Index.jsx
+++ src/Index.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './Pages/HomePage';
 import { App } from './App';
 import { NotFoundPage } from './Pages/NotFoundPage';
+import { LoginPage } from './Pages/LoginPage/LoginPage';
+import { UserPage } from './Pages/UserPage/UserPage';
 
 
 export const Index = () => {
@@ -16,6 +18,14 @@ export const Index = () => {
                 {
                     path: '/home',
                     element: <HomePage></HomePage>,
+                }, 
+                {
+                    path: '/login',
+                    element: <LoginPage></LoginPage>
+                },
+                {
+                    path: '/user',
+                    element: <UserPage></UserPage>
                 }
             ]
         }
