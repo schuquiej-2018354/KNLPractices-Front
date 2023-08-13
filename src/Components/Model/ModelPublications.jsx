9fc5268d91d053e1cbd4d48a6265b3d09f435d59
slopez-2018151 <slopez-2018151@kinal.edu.gt>
2023-08-12 23:37:06 -0600

    Pa la EXPO

--- src/Components/Model/ModelPublications.jsx
+++ src/Components/Model/ModelPublications.jsx
@@ -98,7 +98,7 @@ export const ModelPublications = ({ updateFav, id, _id, idUser, user, userImage,
     }
 
     const report = (id) => {
-        try{
+        try {
             Swal.fire({
                 title: 'Do you want to report this publication?',
                 text: "You won't be able to revert this!",
@@ -119,7 +119,7 @@ export const ModelPublications = ({ updateFav, id, _id, idUser, user, userImage,
                     update();
                 }
             })
-        }catch(e){
+        } catch (e) {
             console.log(e);
         }
     }
@@ -143,7 +143,7 @@ export const ModelPublications = ({ updateFav, id, _id, idUser, user, userImage,
 
     return (
         <>
-            <div className='card bx' style={{backgroundColor: '#f8f8f8'}}>
+            <div className='card bx' style={{ backgroundColor: '#f8f8f8' }}>
                 <div className='row g-0 rounded overflow-hidden flex-md-row h-md-250 position-relative'>
                     <div className='col p-4 d-flex flex-column position-static text-black'>
                         <div className='row align-items-center'>
@@ -153,18 +153,18 @@ export const ModelPublications = ({ updateFav, id, _id, idUser, user, userImage,
                             <div className='col'>
                                 <strong className='d-inline-block mb-2 text-black' onClick={navigateUserPage} style={{ cursor: 'pointer', marginRight: '1rem' }} >@{user}</strong>
                                 {
-                                verified === true ? (
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16" style={{fill: '#0d6efd'}}>
-                                        <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
+                                    verified === true ? (
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16" style={{ fill: '#0d6efd' }}>
+                                            <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                                         </svg>
-                                ) : <></>
-                            }
+                                    ) : <></>
+                                }
                             </div>
                             <div className='col'>
-                                {
-                                    dataUser.id == idUser && type != 'Comment' ? (
-                                        <>
-                                            <div style={{ display: 'flex', justifyContent: 'end' }}>
+                                <div style={{ display: 'flex', justifyContent: 'end' }}>
+                                    {
+                                        dataUser.id == idUser && type != 'Comment' ? (
+                                            <>
                                                 <button className="btn" onClick={(() => deletePublication(id))} style={{ cursor: 'pointer', padding: '0' }}>
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16" style={{ fill: 'black' }}>
                                                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
@@ -177,18 +177,17 @@ export const ModelPublications = ({ updateFav, id, _id, idUser, user, userImage,
                                                     </svg>
                                                 </button>
                                                 &ensp;
-                                                <button className='btn' onClick={()=> report(id)} style={{ cursor: 'pointer', padding: '0' }}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16" style={{fill: 'black'}}>
-                                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                                    <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
-                                                    </svg>
-                                                </button>
-                                            </div>
-                                            <br />
-                                        </>
-
-                                    ) : <></>
-                                }
+                                                <br />
+                                            </>
+                                        ) : <></>
+                                    }
+                                    <button className='btn' onClick={() => report(id)} style={{ cursor: 'pointer', padding: '0' }}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16" style={{ fill: 'black' }}>
+                                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                            <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+                                        </svg>
+                                    </button>
+                                </div>
                                 <div className='mb-1 text-muted text-end' style={{ marginRight: '1rem' }}>
                                     {time}
                                 </div>
